5ad34165dc8b6810ae6f555cecae3f36f9f348b8
yosuke <sugiyama.yosuke.work@gmail.com>
2024-01-04 04:04:03 +0900

    list

---
+++ list.py
@@ -0,0 +1,16 @@
+test_list = ["python","-","izm",".","com"]
+
+for i in test_list:
+    print(i)
+
+test_list_1 = []
+print(test_list_1)
+
+test_list_1.append("a")
+test_list_1.append("i")
+test_list_1.append("u")
+test_list_1.append("e")
+test_list_1.append("o")
+
+for i in test_list_1:
+    print(i)
