62b43f12d887d287211401a1b67d6156532be47f
yosuke <sugiyama.yosuke.work@gmail.com>
2024-01-19 03:20:45 +0900

    sort

---
+++ sort.py
@@ -0,0 +1,18 @@
+python_list = []
+python_list.append(100)
+python_list.append(200)
+python_list.append(10)
+python_list.append(800)
+python_list.append(60)
+
+for i in python_list:
+    print(i)
+
+# python_list.sort()
+
+for i in sorted(python_list):
+    print(i)
+
+
+for i in python_list:
+    print(i)
