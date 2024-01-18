d19b1160e08f7494176c680c6c08acaa88ea6603
yosuke <sugiyama.yosuke.work@gmail.com>
2024-01-19 03:57:49 +0900

    join

---
+++ join.py
@@ -0,0 +1,8 @@
+elem = ['www', 'python-izm', 'com']
+
+print('\n'.join(elem))
+print(','.join(elem))
+print('||'.join('1234567890'))
+
+print('+'.join(('1', '2', '3')))
+# print('+'.join((1, 2, 3)))
